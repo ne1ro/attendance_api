18e301bf9d67a931114209a07502929c2018996b
neiro <neiro.mail@gmail.com>
2018-12-02 11:38:30 +0300

    Run scotty web server

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( server
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "Hello, Haskell!"
+import Web.Scotty
+import Data.Monoid (mconcat)
+
+server :: IO ()
+server = do
+  putStrLn "Starting server ..."
+
+  scotty 3000 $ do
+    get "/:word" $ do
+      beam <- param "word"
+      html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
