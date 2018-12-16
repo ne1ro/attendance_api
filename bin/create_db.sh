8ddd0eef4c320b140c46d4136a041d0aa7d91070
neiro <neiro.mail@gmail.com>
2018-12-16 18:43:40 +0300

    Fix database structure

--- bin/create_db.sh
+++ bin/create_db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-sqlite3 attendance.db "CREATE TABLE attendants (id INTEGER PRIMARY KEY, firstName text, lastName text);\
-  CREATE TABLE attendancies (id INTEGER PRIMARY KEY, attendantId INTEGER, day DATE, status BOOLEAN, FOREIGN KEY(attendantId) REFERENCES attendants(id) ON DELETE CASCADE);\
+sqlite3 attendance.db "CREATE TABLE attendants (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, firstName text NOT NULL, lastName text NOT NULL);\
+  CREATE TABLE attendancies (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, attendantId INTEGER NOT NULL, day DATE NOT NULL, status BOOLEAN, FOREIGN KEY(attendantId) REFERENCES attendants(id) ON DELETE CASCADE);\
 "
