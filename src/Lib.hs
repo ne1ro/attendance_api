e939b0ba1cd0b9a8edbe7fabdef51f70fdc77d04
neiro <neiro.mail@gmail.com>
2018-12-20 20:48:03 +0300

    Refactor router

--- src/Lib.hs
+++ src/Lib.hs
@@ -10,39 +10,45 @@ import           Control.Applicative
 import           Control.Monad
 import           Control.Monad.IO.Class
 import           Data.Monoid            (mconcat)
+import Data.Aeson (object, (.=))
 import           Data.Time.Calendar
 import           Data.Time.Clock
 import           Database.SQLite.Simple
 import           Network.HTTP.Types.Status
 import           Web.Scotty
 
+validationError :: String
+validationError = "Validation Error"
+
+router :: Connection -> ScottyM ()
+router conn = do
+  get "/attendants" $ do
+    attendants <- liftIO $ Application.listAttendants conn
+    json attendants
+
+  post "/attendants" $ do
+    body <- jsonData
+    result <- liftIO $ Application.createAttendant conn body
+    case result of
+      Left err -> json $ object ["message" .= validationError]
+      Right attendant -> json attendant
+
+  defaultHandler $ \str -> do
+    status status500
+    json str
+
+  -- get "/attendancies/:day" $ do
+  --   day <- param "word" :: String
+    -- today <- fmap utctDay getCurrentTime
+  --   json $ Application.listAttendancies day
+
+  -- get "/:word" $ do
+    -- beam <- param "word"
+    -- html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
 server :: IO ()
 server = do
   putStrLn "Starting server ..."
   conn <- open "attendance.db"
-
-  scotty 3000 $ do
-    get "/attendants" $ do
-      attendants <- liftIO $ Application.listAttendants conn
-      json attendants
-
-    post "/attendants" $ do
-      body <- jsonData
-      case liftIO $ Application.createAttendant conn body of
-        Left err -> json "validation error"
-        Right attendant -> json attendant
-
-    defaultHandler $ \str -> do
-      status status500
-      json str
-
-    -- get "/attendancies/:day" $ do
-    --   day <- param "word" :: String
-      -- today <- fmap utctDay getCurrentTime
-    --   json $ Application.listAttendancies day
-
-    -- get "/:word" $ do
-      -- beam <- param "word"
-      -- html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
-
+  scotty 3000 $ router conn
   close conn
